4619dbb3f63580aa5d2e5c7455d923b2ee053dbb
phptuts <noah.php@gmail.com>
2018-07-14 22:04:04 -0700

    Got cannon to rotate with money bag

--- js/game.js
+++ js/game.js
@@ -1,9 +1,12 @@
 let gameScene = new Phaser.Scene('Game');
 
+let oneRadian = Math.PI / 180;
+
 // Runs first and is used to set all the variables
 gameScene.init = function() {
 	this.moneyYears = []; // The number of money invested in that year
 	this.interestRate = .09; // Assuming 9% interest rate
+	this.moneyDistanceFromCannon = -92;
 };
 
 // Runs second and is used to set all the images used in the game
@@ -14,6 +17,7 @@ gameScene.preload = function() {
 	this.load.image('creditcard', 'images/creditcard.png');
 	this.load.image('car', 'images/car.png');
 	this.load.image('eatingout', 'images/eatingout.png');
+	this.load.image('moneybag', 'images/money.png');
 };
 
 // Runs third and is used to position all the images initially in the game
@@ -29,9 +33,11 @@ gameScene.create = function() {
 	
 	// position the cannon and change origin so it rotates around the wheels
 	this.cannon = this.add.sprite(560, 330, 'cannon');
+		this.cannon.setScale(.4);
+
 	this.cannon.setOrigin(113/300, 79/149);
-	this.cannon.setScale(.4);
-	
+		console.log(this.cannon, 'cannon')
+
 	// Bad Guys
 	this.creditCard = this.add.sprite(440, 100, 'creditcard');
 	this.creditCard.setScale(.5);
@@ -48,8 +54,20 @@ gameScene.create = function() {
 	this.eatingout = this.add.sprite(200, 100, 'eatingout');
 	this.eatingout.setScale(.3);
 	this.eatingout.percentTaken = .3;
-
 	
+	this.moneyBag = this.add.sprite(495, 300, 'moneybag');
+	this.moneyBag.setScale(.15);
+	this.moneyBag.firingAngle = 8;
+	this.moneyBag.rotateDirection = function (randianAngle) {
+		Phaser.Actions.RotateAroundDistance(
+			[this], // This takes an array.  Using "this" because of the context.
+			{ x: 560  , y: 320 }, // This is where the wheel is.
+			randianAngle, // You have use radian there like angle but wierder
+			80 // radius aka distance from the pivot point to what is being rotated around
+		);
+	};
+	this.moneyBag.rotateDirection(0);
+		
 	// Keyboard Inputputs
 	this.upKey = 
 		this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
@@ -63,13 +81,21 @@ gameScene.create = function() {
 // conditions and control things in thing in the game
 // This may run 60 times per second and is the last thing that is ran
 gameScene.update = function() {
-	if (this.upKey.isDown) {
+	if (this.upKey.isDown && this.cannon.angle < 85) {
 		this.cannon.angle += 1;
+		this.moneyBag.firingAngle += 1;	
+		this.moneyBag.rotateDirection(oneRadian);
 	}
 	
-	if (this.leftKey.isDown) {
+	if (this.leftKey.isDown && this.cannon.angle > -10) {
 		this.cannon.angle -= 1;
-	}
+		this.moneyBag.firingAngle -= 1;
+		this.moneyBag.rotateDirection(oneRadian * -1);
+	}	
+	
+
+			
+
 };
 
 // set config of game
