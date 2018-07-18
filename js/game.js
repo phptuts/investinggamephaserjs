e34f7eeceb9a239df5fd794b2a7d2f0c61dd4002
phptuts <noah.php@gmail.com>
2018-07-18 10:14:32 -0700

    Got money to shrink and hit.  Need to refactor the code.

--- js/game.js
+++ js/game.js
@@ -6,9 +6,11 @@ const oneRadian = Math.PI / 180;
 gameScene.init = function() {
 	this.moneyYears = []; // The number of money invested in that year
 	this.interestRate = .09; // Assuming 9% interest rate
-	this.moneyDistanceFromCannon = -92;
-	this.firingMoney = false;
-	this.movingSprites = [];
+	this.moneyDistanceFromCannon = -92; // This is the distance away from the cannon
+	this.firingMoney = false; // A flag when a money bag is fired
+	this.movingSprites = []; // Theses are sprites that are moving
+	this.enemySprites = []; // Theses are the enemy sprites
+	this.moneyBagScale = .15; // This is the scale we are setting money bag image
 };
 
 // Runs second and is used to set all the images used in the game
@@ -24,37 +26,37 @@ gameScene.preload = function() {
 
 // Runs third and is used to position all the images initially in the game
 gameScene.create = function() {
-	
+
 	// positions the background
 	let bg = this.add.sprite(0,0, 'background');
 	bg.setOrigin(0,0);
-	
+
 	// position the pig
 	this.pig = this.add.sprite(50, 200, 'pig');
 	this.pig.setScale(.2);
-	
+
 	// position the cannon and change origin so it rotates around the wheels
 	this.cannon = this.add.sprite(560, 330, 'cannon');
-		this.cannon.setScale(.4);
+	this.cannon.setScale(.4);
 
 	this.cannon.setOrigin(113/300, 79/149);
-		console.log(this.cannon, 'cannon')
+	console.log(this.cannon, 'cannon')
 
 	// Bad Guys
 	this.creditCard = this.add.sprite(410, 300, 'creditcard');
 	this.creditCard.setScale(.5);
 	this.creditCard.percentTaken = .3;
-	
+
 	this.car = this.add.sprite(300, 100, 'car');
 	this.car.setScale(.2);
 	this.car.percentTaken = .3;
 
-	this.eatingout = this.add.sprite(150, 100, 'eatingout');
+	this.eatingout = this.add.sprite(170, 100, 'eatingout');
 	this.eatingout.setScale(.3);
 	this.eatingout.percentTaken = .3;
-	
+
 	this.moneyBag = this.physics.add.image(495, 300, 'moneybag');
-	this.moneyBag.setScale(.15);
+	this.moneyBag.setScale(this.moneyBagScale);
 	this.moneyBag.firingAngle = 8;
 	this.moneyBag.rotateDirection = function (randianAngle) {
 		Phaser.Actions.RotateAroundDistance(
@@ -64,34 +66,59 @@ gameScene.create = function() {
 			80 // radius aka distance from the pivot point to what is being rotated around
 		);
 	};
+
+	this.enemySprites.push(this.creditCard);
+	this.enemySprites.push(this.car);
+	this.enemySprites.push(this.eatingout);
 	
 	this.movingSprites.push(this.eatingout);
 	this.movingSprites.push(this.car);
 	this.movingSprites.push(this.creditCard);
 	this.movingSprites.push(this.pig);
 	
+	this.enemySprites.forEach(function(sprite) {
+		sprite.hasBeenHit = false;
+	});
+	
 	this.movingSprites.forEach(function(sprite) {
 		let speedDirection = Math.random() > .5 ? 1 : -1;
 		let speed = Math.random() * 2 + 2 * speedDirection;
 		sprite.speedY = speed;
-	});
-	
+	}, this);
+
 	this.moneyBag.rotateDirection(0);
-		
+
 	// Keyboard Inputputs
 	this.upKey = 
 		this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
 
 	this.leftKey = 
 		this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-		
+
 	this.spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+	
+	this.resetMoney();
 };
+
+gameScene.resetMoney = function () {
+	this.moneyBag.body.gravity.y = 0;
+	this.moneyBag.body.setVelocity(0,0);
+	this.moneyBag.x = 495;
+	this.moneyBag.y = 300;
+	this.moneyBag.setScale(this.moneyBagScale);
+	this.moneyBag.rotateDirection(oneRadian * this.cannon.angle);
+	this.firingMoney = false;
+	
+	this.enemySprites.forEach(function(sprite) {
+		sprite.hasBeenHit = false;
+	});
+}
 
 // Runs after every frame and is used to check 
 // conditions and control thi     ngs in thing in the game
 // This may run 60 times per second and is the last thing that is ran
 gameScene.update = function() {
+
 	if (this.upKey.isDown && this.cannon.angle < 85 ) {
 		this.cannon.angle += 1;
 		this.moneyBag.firingAngle += 1;
@@ -99,7 +126,7 @@ gameScene.update = function() {
 			this.moneyBag.rotateDirection(oneRadian);
 		}
 	}
-	
+
 	if (this.leftKey.isDown && this.cannon.angle > -10) {
 		this.cannon.angle -= 1;
 		this.moneyBag.firingAngle -= 1;
@@ -107,41 +134,60 @@ gameScene.update = function() {
 			this.moneyBag.rotateDirection(oneRadian * -1);
 		}
 	}	
-	
-	
-	if (this.spaceBar.isDown) {
-    	this.physics.velocityFromRotation(
+
+
+	if (this.spaceBar.isDown && !this.firingMoney) {
+		this.physics.velocityFromRotation(
 			this.moneyBag.firingAngle * oneRadian,
-			-200,
+			-250,
 			this.moneyBag.body.velocity
 		);
 		this.moneyBag.body.gravity.y = 50;
 		this.firingMoney = true;
 	}
-	
-	if (this.moneyBag.x <= 10 || this.moneyBag.y > 650) {
-		this.moneyBag.body.gravity.y = 0;
-		this.moneyBag.body.setVelocity(0,0);
-		this.moneyBag.x = 495;
-		this.moneyBag.y = 300;
-		this.moneyBag.rotateDirection(oneRadian * this.cannon.angle);
-		this.firingMoney = false;
+
+	if (this.moneyBag.x <= 10 || this.moneyBag.y > 650 || this.moneyBag.y < 10) {
+		gameScene.resetMoney();
 	}
-	
-	
+
+
+
 	this.movingSprites.forEach(function(sprite) {
 		if (sprite.y <= 10) {
 			sprite.y = 11;
 			sprite.speedY *= -1;
 		}
-		
+
 		if (sprite.y >= 300) {
 			sprite.y = 299;
 			sprite.speedY *= -1;	
 		}
 
 		sprite.y += sprite.speedY;
-	});		
+	});	
+
+	this.enemySprites.forEach(function(sprite) {
+		let hasCollisionOccurred = Phaser.Geom.Intersects.RectangleToRectangle(
+			sprite.getBounds(), 
+			this.moneyBag.getBounds()
+		);
+		if (hasCollisionOccurred && !sprite.hasBeenHit) {
+			this.moneyBag.setScale(this.moneyBag.scaleY *.8);
+			sprite.hasBeenHit = true;
+			console.log('lost some money');
+		}
+	}, this);
+
+	let hasMoneyHitPig = Phaser.Geom.Intersects.RectangleToRectangle(
+			this.pig.getBounds(), 
+			this.moneyBag.getBounds()
+		);
+	
+	if (hasMoneyHitPig) {
+		console.log('Money Money');
+		this.resetMoney();
+	}
+
 
 };
 
@@ -152,12 +198,12 @@ let config = {
 	height: 360,
 	scene: gameScene,
 	physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { z: 300 },
-            debug: true
-        }
-    }
+		default: 'arcade',
+		arcade: {
+			gravity: { z: 300 },
+			debug: true
+		}
+	}
 };
 
 // create an new game and pass configuration to it
